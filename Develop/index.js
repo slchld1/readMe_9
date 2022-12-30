758cd6cd8ba4f2148cca7aa334923af0918dc355
slchld1 <jaecho203@gmail.com>
2022-12-30 04:52:03 -0500

    changes

--- Develop/index.js
+++ Develop/index.js
@@ -2,17 +2,33 @@
 const inquirer = require('inquirer');
 const MarkDown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
+const { resolveObjectURL } = require('buffer');
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
         message: 'Please enter the desired Project Title:',
+        validate: function (output) {
+            if(output.length === 0) {
+                return console.log('Title is required.')
+            }else{
+                return true;
+            }
+        }
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Please enter the Description of the Project:'
+        message: 'Please enter the Description of the Project:',
+        validate: function (output) {
+            if (output.length === 0){
+                return console.log("Description is required.")
+
+            }else {
+                return true
+            }
+        }
     },
     {
         type: 'input',
@@ -33,33 +49,55 @@ const questions = [
         type: 'input',
         name: 'email',
         message: 'Please enter your Email:',
+        validate: function (output) {
+            if (output.length === 0){
+                return console.log('Email is required.')
+            }else {
+                return true;
+            }
+        }
     },
     {
         type: 'input',
         name: 'github',
-        message: 'Please enter your GitHub:',
+        message: 'Please enter the name of your GitHub repo:',
+        validate: function (output) {
+            if (output === 0) {
+                return console.log('GitHub repo is required.')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide any tests written:',
     },
     {
         type: 'checkbox',
         name: 'license',
         message: 'Choose your License:',
-        choices: ['MIT', 'ISC', 'Others', 'Unknown'],
+        choices: ['MIT', 'ISC', 'Mozilla Public License 2.0', 'Unlicensed'],
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+        fs.writeFile('README.md', data, error => {
+            if (error){
+                return console.log(error);
+            }else{
+                console.log("Success!");
+            }
+        });
 }
 
-
+// const createFile = utils.promisify(writeToFile)
 // TODO: Create a function to initialize app
 function init() {
     return inquirer.prompt(questions)
     .then((data) => {
         const mark = MarkDown.generateReadMe(data)
-        fs.writeFile('README.md', mark, (err) => {
-            err ? console.log(err) : console.log('Success!')
-        });
+        writeToFile(`README.md`, mark)
         return data;
     })
     .catch((error) => {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -39,30 +39,17 @@ class MarkDown {
 
 }
 // If there is no license, return an empty string
-// function renderLicenseBadge(license) {
-//   if(license == null){
-//     return ""
-//   }
-// }
+function renderLicenseBadge(license) {}
 
-// // TODO: Create a function that returns the license link
-// // If there is no license, return an empty string
-// function renderLicenseLink(license) {
-//   if(license == "MIT"){
-//     return "MIT"
-//   }
-// }
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
 
-// // TODO: Create a function that returns the license section of README
-// // If there is no license, return an empty string
-// function renderLicenseSection(license) {
-// }
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
 
-// // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
-
-// `;
-// }
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {}
 
 module.exports = MarkDown;
